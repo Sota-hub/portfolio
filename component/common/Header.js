fbb97de9e43e7b9c7998a508c9569a42ab860248
SOTA <sota00@outlook.jp>
2021-11-17 15:27:55 -0800

    add responsive style

--- component/common/Header.js
+++ component/common/Header.js
@@ -7,44 +7,47 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <header className="container">
-      <div className={classes.grid_container}>
-        <Link href="/">
-          <a
-            className={`${classes.grid_items} ${
-              router.pathname === "/" && classes.active
-            }`}
-          >
-            Home
-          </a>
-        </Link>
-        <Link href="/about">
-          <a
-            className={`${classes.grid_items} ${
-              router.pathname === "/about" && classes.active
-            }`}
-          >
-            About
-          </a>
-        </Link>
-        <Link href="/works">
-          <a
-            className={`${classes.grid_items} ${
-              router.pathname === "/works" && classes.active
-            }`}
-          >
-            Works
-          </a>
-        </Link>
-        <Link href="/contact">
-          <a
-            className={`${classes.grid_items} ${
-              router.pathname === "/contact" && classes.active
-            }`}
-          >
-            Contact
-          </a>
-        </Link>
+    <header>
+      <div className={`container ${classes.header_container}`}>
+        <h2 className={classes.header_icon}>portfolio</h2>
+        <div className={classes.grid_container}>
+          <Link href="/">
+            <a
+              className={`${classes.grid_items} ${
+                router.pathname === "/" && classes.active
+              }`}
+            >
+              Home
+            </a>
+          </Link>
+          <Link href="/about">
+            <a
+              className={`${classes.grid_items} ${
+                router.pathname === "/about" && classes.active
+              }`}
+            >
+              About
+            </a>
+          </Link>
+          <Link href="/works">
+            <a
+              className={`${classes.grid_items} ${
+                router.pathname === "/works" && classes.active
+              }`}
+            >
+              Works
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a
+              className={`${classes.grid_items} ${
+                router.pathname === "/contact" && classes.active
+              }`}
+            >
+              Contact
+            </a>
+          </Link>
+        </div>
       </div>
     </header>
   );
--- component/home/GuideList.js
+++ component/home/GuideList.js
@@ -7,34 +7,38 @@ const GuideList = () => {
   return (
     <div className="container">
       <ul className={classes.guideList_container}>
-        <p className={classes.message_style}>Are you interested in me?</p>
-        <li className={classes.link_container}>
-          <Link href="/about" passHref>
-            <a>
-              <Guide title="ABOUT ME" path="/aboutMe.jpg" />
-            </a>
-          </Link>
-        </li>
-        <p className={classes.message_style}>
-          NOT interested in me? -<br /> Please see my works at least!
-        </p>
-        <li className={classes.link_container}>
-          <Link href="/works" passHref>
-            <a>
-              <Guide title="WORKS" path="/works.jpg" />
-            </a>
-          </Link>
-        </li>
-        <p className={classes.message_style}>
-          Is it the time to hire me already?
-        </p>
-        <li className={classes.link_container}>
-          <Link href="/contact" passHref>
-            <a>
-              <Guide title="CONTACT" path="/contact.jpg" />
-            </a>
-          </Link>
-        </li>
+        <div className={classes.vertical_grid}>
+          <p className={classes.message_style}>Are you interested in me?</p>
+          <li className={classes.link_container}>
+            <Link href="/about" passHref>
+              <a>
+                <Guide title="ABOUT ME" path="/aboutMe.jpg" />
+              </a>
+            </Link>
+          </li>
+        </div>
+        <div className={classes.vertical_grid}>
+          <p className={classes.message_style}>Please see my works at least!</p>
+          <li className={classes.link_container}>
+            <Link href="/works" passHref>
+              <a>
+                <Guide title="WORKS" path="/works.jpg" />
+              </a>
+            </Link>
+          </li>
+        </div>
+        <div className={classes.vertical_grid}>
+          <p className={classes.message_style}>
+            Is it the time to hire me already?
+          </p>
+          <li className={classes.link_container}>
+            <Link href="/contact" passHref>
+              <a>
+                <Guide title="CONTACT" path="/contact.jpg" />
+              </a>
+            </Link>
+          </li>
+        </div>
       </ul>
     </div>
   );
