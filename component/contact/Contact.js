25cf94eb0baee06db0d9640e7fe08db4d6f34a11
SOTA <sota00@outlook.jp>
2021-11-17 20:52:42 -0800

    environment variable doesn't work

--- component/contact/Contact.js
+++ component/contact/Contact.js
@@ -29,7 +29,7 @@ const Contact = () => {
 
   const submitHandler = async (content) => {
     try {
-      const response = await fetch("/pages/api/mailer", {
+      const response = await fetch("/api/mailer", {
         method: "POST",
         headers: {
           Accept: "application/json",
